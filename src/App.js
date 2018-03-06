c509c54b66e55bc36c83ac926e983e7674c3bdb7
Gavin Smith <gavinasmith@me.com>
2018-03-06 13:27:18 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">
+            A* 8-Puzzle Solver<br/>
+          </h1>
+          <h3>
+            <small>
+              Kennesaw State University CS4242 Spring 2018<br/>
+              Assignment 02 Solution by Gavin Smith
+            </small>
+          </h3>
+        </header>
+        <p className="App-intro">
+          
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
